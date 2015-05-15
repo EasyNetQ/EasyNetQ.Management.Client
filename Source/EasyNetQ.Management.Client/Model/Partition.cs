fcaf79221f0183a37f20919fbc9d4fe542ffd541
ryanchappell <ryan.p.chap@gmail.com>
2015-05-15 08:31:20 -0700

    adding Partition model and Node.Partitions prop
    Context:
    https://groups.google.com/forum/#!msg/easynetq/vXLOR8NelSo/KJ7zePoNVH0J

---
+++ Source/EasyNetQ.Management.Client/Model/Partition.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EasyNetQ.Management.Client.Model
+{
+    public class Partition
+    {
+    }
+}
