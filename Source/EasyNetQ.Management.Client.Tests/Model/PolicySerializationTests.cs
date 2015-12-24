995ec954468107f0af20071cc736cf1ee9018d50
NikGovorov <nikita.govorov@gmail.com>
2015-12-24 13:58:37 +1000

    Implemented support of 'apply-to' property for Policy object and 'federation-upstream' property for PolicyDefinition object.

--- Source/EasyNetQ.Management.Client.Tests/Model/PolicySerializationTests.cs
+++ Source/EasyNetQ.Management.Client.Tests/Model/PolicySerializationTests.cs
@@ -18,9 +18,37 @@
         }
 
         [Test]
+        public void Should_read_apply_to_properly()
+        {
+            var exactlyPolicies = _policy.Where(p => p.Name == "ha-duplicate").ToList();
+            Assert.AreEqual(1, exactlyPolicies.Count);
+            var policy = exactlyPolicies.First();
+            Assert.AreEqual(ApplyMode.Queues, policy.ApplyTo);
+
+            exactlyPolicies = _policy.Where(p => p.Name == "mirror_test").ToList();
+            Assert.AreEqual(1, exactlyPolicies.Count);
+            policy = exactlyPolicies.First();
+            Assert.AreEqual(ApplyMode.Exchanges, policy.ApplyTo);
+
+            exactlyPolicies = _policy.Where(p => p.Name == "default apply to").ToList();
+            Assert.AreEqual(1, exactlyPolicies.Count);
+            policy = exactlyPolicies.First();
+            Assert.AreEqual(ApplyMode.All, policy.ApplyTo);   
+        }
+
+           [Test]
+        public void Should_read_federation_upstream_properly()
+        {
+            var exactlyPolicies = _policy.Where(p => p.Name == "mirror_test").ToList();
+            Assert.AreEqual(1, exactlyPolicies.Count);
+            var policy = exactlyPolicies.First();
+            Assert.AreEqual("test", policy.Definition.FederationUpstream);
+        }
+
+        [Test]
         public void Should_read_exactly_ha_properly()
         {
-            _policy.Count().ShouldEqual(2);
+            _policy.Count().ShouldEqual(3);
             var exactlyPolicies = _policy.Where(p => p.Name == "ha-duplicate");
             Assert.AreEqual(1, exactlyPolicies.Count());
             var policy = exactlyPolicies.First();
@@ -35,7 +63,7 @@
         [Test]
         public void Should_read_nodes_ha_properly()
         {
-            _policy.Count().ShouldEqual(2);
+            _policy.Count().ShouldEqual(3);
             var mirrorTestPolicies = _policy.Where(p => p.Name == "mirror_test");
             Assert.AreEqual(1, mirrorTestPolicies.Count());
             var policy = mirrorTestPolicies.First();
@@ -45,6 +73,48 @@
             Assert.AreEqual("mirror", policy.Pattern);
             Assert.AreEqual(HaSyncMode.Automatic, policy.Definition.HaSyncMode);
             Assert.AreEqual(0, policy.Priority);
+        }
+
+        [Test]
+        public void Should_write_apply_to_properly()
+        {
+            var serializedMessage = JsonConvert.SerializeObject(new Policy
+            {
+                Name = "bob",
+                Pattern = "foo"
+            }, ManagementClient.Settings);
+            
+            Assert.IsTrue(serializedMessage.Contains("\"apply-to\":\"all\""));
+
+            serializedMessage = JsonConvert.SerializeObject(new Policy
+            {
+                Name = "bob",
+                Pattern = "foo",
+                ApplyTo = ApplyMode.Exchanges
+            }, ManagementClient.Settings);
+
+            Assert.IsTrue(serializedMessage.Contains("\"apply-to\":\"exchanges\""));
+
+            serializedMessage = JsonConvert.SerializeObject(new Policy
+            {
+                Name = "bob",
+                Pattern = "foo",
+                ApplyTo = ApplyMode.Queues
+            }, ManagementClient.Settings);
+            Assert.IsTrue(serializedMessage.Contains("\"apply-to\":\"queues\""));
+        }
+
+        [Test]
+        public void Should_write_federation_upstream_properly()
+        {
+            var serializedMessage = JsonConvert.SerializeObject(new Policy
+            {
+                Name = "bob",
+                Pattern = "foo",
+                Definition = new PolicyDefinition { FederationUpstream = "my-upstream"}
+            }, ManagementClient.Settings);
+
+            Assert.IsTrue(serializedMessage.Contains("\"federation-upstream\":\"my-upstream\""));
         }
 
         [Test]
--- Source/EasyNetQ.Management.Client/Model/Policy.cs
+++ Source/EasyNetQ.Management.Client/Model/Policy.cs
@@ -1,4 +1,6 @@
-﻿namespace EasyNetQ.Management.Client.Model
+﻿using Newtonsoft.Json;
+
+namespace EasyNetQ.Management.Client.Model
 {
     public class Policy
     {
@@ -6,6 +8,8 @@
         public string Name { get; set; }
         public string Pattern { get; set; }
         public PolicyDefinition Definition { get; set; }
+        [JsonProperty("apply-to", NullValueHandling = NullValueHandling.Ignore)]
+        public ApplyMode ApplyTo { get; set; }
         public int Priority { get; set; }
     }
 }
