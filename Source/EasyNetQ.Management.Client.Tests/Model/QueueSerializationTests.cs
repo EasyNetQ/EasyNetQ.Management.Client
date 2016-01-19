6d61753708fd16c4a7643c16b272e3214f315f10
NikGovorov <nikita.govorov@gmail.com>
2016-01-19 10:28:41 +1000

    Implemented json converter and marked PeerPort property with it for cases when peer_port is not a number.

--- Source/EasyNetQ.Management.Client.Tests/Model/QueueSerializationTests.cs
+++ Source/EasyNetQ.Management.Client.Tests/Model/QueueSerializationTests.cs
@@ -15,9 +15,30 @@ namespace EasyNetQ.Management.Client.Tests.Model
         public void BackingQueueStatus_With_NextSeqId_Exceeding_IntMaxLength_Can_Be_Deserialized()
         {
             queues = ResourceLoader.LoadObjectFromJson<List<Queue>>("Queues.json");
+
             var queue = queues[1];
 
             queue.BackingQueueStatus.NextSeqId.ShouldEqual(((long)int.MaxValue) + 1);
+        }
+
+        [Test]
+        public void NonInt_Consumer_Details_Channel_Details_Peer_Port_Can_Be_Deserialize()
+        {
+            queues = ResourceLoader.LoadObjectFromJson<List<Queue>>("Queues.json");
+
+            var queue = queues[2];
+
+            queue.ConsumerDetails[0].ChannelDetails.PeerPort.ShouldEqual(0);
+        }
+
+        [Test]
+        public void Int_Consumer_Details_Channel_Details_Peer_Port_Can_Be_Deserialize()
+        {
+            queues = ResourceLoader.LoadObjectFromJson<List<Queue>>("Queues.json");
+
+            var queue = queues[3];
+
+            queue.ConsumerDetails[0].ChannelDetails.PeerPort.ShouldEqual(9861);
         }
     }
 }
