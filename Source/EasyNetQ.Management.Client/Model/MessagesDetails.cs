b80083a3eeb4cdc7cfc424c0976f4e288e54565c
Anders Ljusberg <andlju@gmail.com>
2016-04-06 21:13:42 +0200

    Actually, rate/avg etc should be doubles

--- Source/EasyNetQ.Management.Client/Model/MessagesDetails.cs
+++ Source/EasyNetQ.Management.Client/Model/MessagesDetails.cs
@@ -5,15 +5,15 @@ namespace EasyNetQ.Management.Client.Model
 {
     public class MessagesDetails
     {
-        public long Rate { get; set; }
+        public double Rate { get; set; }
 
         [Obsolete("The Interval and LastEvent properties were removed in RabbitMQ v3.1")]
         public long Interval { get; set; }
         [Obsolete("The Interval and LastEvent properties were removed in RabbitMQ v3.1")]
         public long LastEvent { get; set; }
 
-        public long AvgRate { get; set; }
-        public long Avg { get; set; }
+        public double AvgRate { get; set; }
+        public double Avg { get; set; }
 
         public List<MessagesDetailSample> Samples { get; set; }
     }
