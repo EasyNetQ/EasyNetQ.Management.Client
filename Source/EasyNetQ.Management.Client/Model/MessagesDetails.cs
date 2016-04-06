52e66d3725f93325df8e1b94d473bba8e3a19b6e
Anders Ljusberg <andlju@gmail.com>
2016-04-06 20:43:22 +0200

    Bring back Interval/LastEvent as Obsolete

--- Source/EasyNetQ.Management.Client/Model/MessagesDetails.cs
+++ Source/EasyNetQ.Management.Client/Model/MessagesDetails.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace EasyNetQ.Management.Client.Model
@@ -5,8 +6,15 @@ namespace EasyNetQ.Management.Client.Model
     public class MessagesDetails
     {
         public long Rate { get; set; }
+
+        [Obsolete("The Interval and LastEvent properties were removed in RabbitMQ v3.1")]
+        public long Interval { get; set; }
+        [Obsolete("The Interval and LastEvent properties were removed in RabbitMQ v3.1")]
+        public long LastEvent { get; set; }
+
         public long AvgRate { get; set; }
         public long Avg { get; set; }
+
         public List<MessagesDetailSample> Samples { get; set; }
     }
 }
