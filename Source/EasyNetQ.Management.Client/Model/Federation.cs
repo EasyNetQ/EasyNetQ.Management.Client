b1552e56a01294f487546c830bccffd464355f57
Alper Hankendi <alper.hankendi@hepsiburada.com>
2017-07-10 20:39:42 +0300

    federation model class improvement

--- Source/EasyNetQ.Management.Client/Model/Federation.cs
+++ Source/EasyNetQ.Management.Client/Model/Federation.cs
@@ -1,3 +1,5 @@
+using System;
+using EasyNetQ.Management.Client.Serialization;
 using Newtonsoft.Json;
 
 namespace EasyNetQ.Management.Client.Model
@@ -6,15 +8,18 @@ namespace EasyNetQ.Management.Client.Model
     {
         public string Node { get; set; }
         public string Exchange { get; set; }
-        public string Upstream_exchange { get; set; }
+        public string UpstreamExchange { get; set; }
         public string Type { get; set; }
         public string Vhost { get; set; }
         public string Upstream { get; set; }
+
+        [JsonProperty("id")]
         public string Id { get; set; }
         [JsonProperty("status", NullValueHandling = NullValueHandling.Ignore)]
         public FederationStatus Status { get; set; }
-        public string Local_connection { get; set; }
+        public string LocalConnection { get; set; }
         public string Uri { get; set; }
-        public string Timestamp { get; set; }
+
+        public DateTime Timestamp { get; set; }
     }
 }
