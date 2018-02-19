dd121dea85700d436707c0fe9a3bd285216d8194
James Naylor <jamesnaylor85@gmail.com>
2018-02-19 10:50:52 +0000

    Fix consumer arguments (#65)

---
+++ Source/EasyNetQ.Management.Client/Model/ConsumerArguments.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace EasyNetQ.Management.Client.Model
+{
+    public class ConsumerArguments
+    {
+        [JsonProperty(PropertyName = "x-credit")]
+        public CreditArgument Credit { get; set; }
+    }
+
+    public class CreditArgument
+    {
+        public int Credit { get; set; }
+        public bool Drain { get; set; }
+    }
+}
