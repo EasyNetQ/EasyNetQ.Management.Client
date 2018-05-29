b3dfd86d4605d81ed3d45d36480fbeef19c50fb0
Luigi Berrettini <luigi.berrettini@gmail.com>
2018-05-29 23:22:38 +0100

    Preserve case on dictionary serialization

--- Source/EasyNetQ.Management.Client/RabbitContractResolver.cs
+++ Source/EasyNetQ.Management.Client/RabbitContractResolver.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System;
+using System.Text.RegularExpressions;
 using Newtonsoft.Json.Serialization;
 
 namespace EasyNetQ.Management.Client
@@ -8,6 +9,13 @@ namespace EasyNetQ.Management.Client
         protected override string ResolvePropertyName(string propertyName)
         {
             return Regex.Replace(propertyName, "([a-z])([A-Z])", "$1_$2").ToLower();
+        }
+
+        protected override JsonDictionaryContract CreateDictionaryContract(Type objectType)
+        {
+            var contract = base.CreateDictionaryContract(objectType);
+            contract.DictionaryKeyResolver = propertyName => propertyName;
+            return contract;
         }
     }
 }
