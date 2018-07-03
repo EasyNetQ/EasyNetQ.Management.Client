50171d74746a7067d1bb20a6fc81a6ed5dcc375d
Luigi Berrettini <luigi.berrettini@gmail.com>
2018-07-03 22:11:21 +0100

    Support ack mode (#85 closes #62)

---
+++ Source/EasyNetQ.Management.Client/Model/Ackmodes.cs
@@ -0,0 +1,10 @@
+﻿namespace EasyNetQ.Management.Client.Model
+{
+    public enum Ackmodes
+    {
+        ack_requeue_false,
+        ack_requeue_true,
+        reject_requeue_false,
+        reject_requeue_true
+    }
+}
--- Source/EasyNetQ.Management.Client/Model/GetMessagesCriteria.cs
+++ Source/EasyNetQ.Management.Client/Model/GetMessagesCriteria.cs
@@ -6,24 +6,21 @@
     public class GetMessagesCriteria
     {
         public long Count { get; private set; }
-        public bool Requeue { get; private set; }
+        public string Ackmode { get; private set; }
         public string Encoding { get; private set; }
 
         /// <summary>
         /// Constructor
         /// </summary>
         /// <param name="count">
-        /// controls the number of messages to get. You may get fewer messages than this 
-        /// if the queue cannot immediately provide them.
+        /// Controls the number of messages to get. You may get fewer messages than this if the queue cannot immediately provide them.
         /// </param>
-        /// <param name="requeue">
-        /// determines whether the messages will be removed from the queue. If requeue 
-        /// is true they will be requeued - but their position in the queue may change 
-        /// and their redelivered flag will be set.
+        /// <param name="ackmode">
+        /// Determines if the message(s) should be placed back into the queue.
         /// </param>
-        public GetMessagesCriteria(long count, bool requeue)
+        public GetMessagesCriteria(long count, Ackmodes ackmode)
         {
-            Requeue = requeue;
+            Ackmode = ackmode.ToString();
             Count = count;
             Encoding = "auto";
         }
