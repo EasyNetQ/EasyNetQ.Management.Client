75fda80b03f457e69fc37bd84ce61244317868d3
Guillaume Deroy <gderoy@dotvision.com>
2018-12-26 17:56:09 +0100

    Support for topic-permissions on management api.

---
+++ Source/EasyNetQ.Management.Client/Model/TopicPermission.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyNetQ.Management.Client.Model
+{
+    public class TopicPermission
+    {
+        public string User { get; set; }
+        public string Vhost { get; set; }
+        public string Exchange { get; set; }
+        public string Write { get; set; }
+        public string Read { get; set; }
+    }
+}
