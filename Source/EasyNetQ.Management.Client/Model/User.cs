050b51e776257940530b532f8348307e2d82b7e0
kmatyaszek <kazimierz.matyaszek@gmail.com>
2022-05-24 14:44:12 +0200

    Problem with CreateUser (Newtonsoft.Json.JsonReaderException: Unexpected character encountered while parsing value: [. Path 'tags', line 1, position 159.) (#153)
    Based on return Tags property type from RabbitMQ converter StringOrArrayConverter will convert data to comma delimited string.

--- Source/EasyNetQ.Management.Client/Model/User.cs
+++ Source/EasyNetQ.Management.Client/Model/User.cs
@@ -1,9 +1,13 @@
+using EasyNetQ.Management.Client.Serialization;
+using Newtonsoft.Json;
+
 namespace EasyNetQ.Management.Client.Model
 {
     public class User
     {
         public string Name { get; set; }
         public string PasswordHash { get; set; }
+        [JsonConverter(typeof(StringOrArrayConverter))]
         public string Tags { get; set; }
     }
 }
