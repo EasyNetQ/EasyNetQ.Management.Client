d2f89a54b147ec049acc11381589abddc1cfecc9
Yury Pliner <yury.pliner@gmail.com>
2022-12-11 19:17:31 +0000

    Convert properties to CamelCase

--- Source/EasyNetQ.Management.Client/Model/ParameterShovelValue.cs
+++ Source/EasyNetQ.Management.Client/Model/ParameterShovelValue.cs
@@ -12,7 +12,7 @@ public class ParameterShovelValue
     /// Note that this field can either be a string, or a list of strings. If more than one string is provided, the shovel will randomly pick one URI from the list. This can be used to connect to a cluster and ensure the link will eventually find another node in the event that one fails. It's probably not a great idea to use multiple URIs that do not point to the same cluster.
     /// </summary>
     [JsonProperty(PropertyName = "src-uri")]
-    public string srcuri { get; set; }
+    public string SrcUri { get; set; }
 
     /// <summary>
     /// The exchange from which to consume. Either this or src-queue (but not both) must be set.
@@ -20,54 +20,20 @@ public class ParameterShovelValue
     /// If the source exchange does not exist on the source broker, it will be not declared; the shovel will fail to start.
     /// </summary>
     [JsonProperty(PropertyName = "src-exchange")]
-    public string srcexchange { get; set; }
+    public string SrcExchange { get; set; }
 
     /// <summary>
     /// Routing key when using src-exchange
     /// </summary>
     [JsonProperty(PropertyName = "src-exchange-key")]
-    public string srcexchangekey { get; set; }
+    public string SrcExchangeKey { get; set; }
 
     /// <summary>
     /// The queue from which to consume. Either this or src-exchange (but not both) must be set.
     /// If the source queue does not exist on the source broker, it will be declared as a durable queue with no arguments.
     /// </summary>
     [JsonProperty(PropertyName = "src-queue")]
-    public string srcqueue { get; set; }
-
-    /// <summary>
-    /// The AMQP URI(s) for the destination. Mandatory. See src-uri above.
-    /// </summary>
-    [JsonProperty(PropertyName = "dest-uri")]
-    public string desturi { get; set; }
-
-    /// <summary>
-    /// The exchange to which messages should be published. Either this or dest-queue (but not both) may be set.
-    /// If the destination exchange does not exist on the source broker, it will be not declared; the shovel will fail to start.
-    /// </summary>
-    [JsonProperty(PropertyName = "dest-exchange")]
-    public string destexchange { get; set; }
-
-    /// <summary>
-    /// The queue to which messages should be published. Either this or dest-exchange (but not both) may be set. If neither is set then messages are republished with their original exchange and routing key.
-    /// If the destination queue does not exist on the source broker, it will be declared as a durable queue with no arguments.
-    /// </summary>
-    [JsonProperty(PropertyName = "dest-queue")]
-    public string destqueue { get; set; }
-
-    /// <summary>
-    /// Determines how the shovel should acknowledge messages. If set to on-confirm (the default), messages are acknowledged to the source broker after they have been confirmed by the destination. This handles network errors and broker failures without losing messages, and is the slowest option.
-    /// If set to on-publish, messages are acknowledged to the source broker after they have been published at the destination. This handles network errors without losing messages, but may lose messages in the event of broker failures.
-    /// If set to no-ack, message acknowledgements are not used. This is the fastest option, but may lose messages in the event of network or broker failures.
-    /// </summary>
-    [JsonProperty(PropertyName = "ack-mode")]
-    public string ackmode { get; set; }
-
-    /// <summary>
-    /// Whether to add x-shovelled headers to the shovelled messages indicating where they have been shovelled from and to. Default is false.
-    /// </summary>
-    [JsonProperty(PropertyName = "add-forward-headers")]
-    public string addforwardheaders { get; set; }
+    public string SrcQueue { get; set; }
 
     /// <summary>
     /// Determines when (if ever) the shovel should delete itself. This can be useful if the shovel is being treated as more of a move operation - i.e. being used to move messages from one queue to another on an ad hoc basis.
@@ -75,6 +41,40 @@ public class ParameterShovelValue
     /// If set to queue-length then the shovel will measure the length of the source queue when starting up, and delete itself after it has transfered that many messages.
     /// If set to an integer, then the shovel will transfer that number of messages before deleting itself.
     /// </summary>
-    [JsonProperty(PropertyName = "delete-after")]
-    public string deleteafter { get; set; }
+    [JsonProperty(PropertyName = "src-delete-after")]
+    public string SrcDeleteAfter { get; set; }
+
+    /// <summary>
+    /// The AMQP URI(s) for the destination. Mandatory. See src-uri above.
+    /// </summary>
+    [JsonProperty(PropertyName = "dest-uri")]
+    public string DestUri { get; set; }
+
+    /// <summary>
+    /// The exchange to which messages should be published. Either this or dest-queue (but not both) may be set.
+    /// If the destination exchange does not exist on the source broker, it will be not declared; the shovel will fail to start.
+    /// </summary>
+    [JsonProperty(PropertyName = "dest-exchange")]
+    public string DestExchange { get; set; }
+
+    /// <summary>
+    /// The queue to which messages should be published. Either this or dest-exchange (but not both) may be set. If neither is set then messages are republished with their original exchange and routing key.
+    /// If the destination queue does not exist on the source broker, it will be declared as a durable queue with no arguments.
+    /// </summary>
+    [JsonProperty(PropertyName = "dest-queue")]
+    public string DestQueue { get; set; }
+
+    /// <summary>
+    /// Determines how the shovel should acknowledge messages. If set to on-confirm (the default), messages are acknowledged to the source broker after they have been confirmed by the destination. This handles network errors and broker failures without losing messages, and is the slowest option.
+    /// If set to on-publish, messages are acknowledged to the source broker after they have been published at the destination. This handles network errors without losing messages, but may lose messages in the event of broker failures.
+    /// If set to no-ack, message acknowledgements are not used. This is the fastest option, but may lose messages in the event of network or broker failures.
+    /// </summary>
+    [JsonProperty(PropertyName = "ack-mode")]
+    public string AckMode { get; set; }
+
+    /// <summary>
+    /// Whether to add x-shovelled headers to the shovelled messages indicating where they have been shovelled from and to. Default is false.
+    /// </summary>
+    [JsonProperty(PropertyName = "add-forward-headers")]
+    public string AddForwardHeaders { get; set; }
 }
