a63a5737c0f9c96f87b955d2f6e2aac70a9fbdfa
Yury Pliner <yury.pliner@gmail.com>
2022-12-24 20:26:44 +0000

    Fix

--- Source/EasyNetQ.Management.Client.Tests/Model/ExchangeInfoTests.cs
+++ Source/EasyNetQ.Management.Client.Tests/Model/ExchangeInfoTests.cs
@@ -15,12 +15,6 @@ public class ExchangeInfoTests
         var exchangeInfo = new ExchangeInfo(expectedName, "direct");
         exchangeInfo.Name.Should().Be(expectedName);
     }
-
-    [Fact]
-    public void Should_throw_if_we_try_to_create_an_exchange_with_an_unknown_type()
-    {
-        Assert.Throws<EasyNetQManagementException>(() => new ExchangeInfo("management_api_test_exchange", "some_bullshit_exchange_type"));
-    }
 }
 
 // ReSharper restore InconsistentNaming
