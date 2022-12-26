d60f613f3aca3f1a03a31879129a12a6ac920c6d
Iurii Pliner <yury.pliner@gmail.com>
2022-12-26 15:39:18 +0000

    Add PayloadEncoding enum (#197)
    * Add PayloadEncoding enum
    
    * Fix rebase

--- Source/EasyNetQ.Management.Client/Model/PublishInfo.cs
+++ Source/EasyNetQ.Management.Client/Model/PublishInfo.cs
@@ -1,35 +1,22 @@
 ﻿namespace EasyNetQ.Management.Client.Model;
 
-#nullable disable
-
 public class PublishInfo
 {
-    public IDictionary<string, object> Properties { get; private set; }
-    public string RoutingKey { get; private set; }
-    public string Payload { get; private set; }
-    public string PayloadEncoding { get; private set; }
+    public IDictionary<string, object> Properties { get; }
+    public string RoutingKey { get; }
+    public string Payload { get; }
+    public PayloadEncoding PayloadEncoding { get; }
 
-    private readonly ISet<string> payloadEncodings = new HashSet<string> { "string", "base64" };
-
-    public PublishInfo(IDictionary<string, object> properties, string routingKey, string payload, string payloadEncoding)
+    public PublishInfo(IDictionary<string, object> properties, string routingKey, string payload, PayloadEncoding payloadEncoding)
     {
-        if (payloadEncoding == null)
-        {
-            throw new ArgumentNullException(nameof(payloadEncoding));
-        }
-        if (!payloadEncodings.Contains(payloadEncoding))
-        {
-            throw new ArgumentException($"payloadEncoding must be one of: '{string.Join(", ", payloadEncodings)}'");
-        }
-
-        Properties = properties ?? throw new ArgumentNullException(nameof(properties));
-        RoutingKey = routingKey ?? throw new ArgumentNullException(nameof(routingKey));
-        Payload = payload ?? throw new ArgumentNullException(nameof(payload));
+        Properties = properties;
+        RoutingKey = routingKey;
+        Payload = payload;
         PayloadEncoding = payloadEncoding;
     }
 
-    public PublishInfo(string routingKey, string payload) :
-        this(new Dictionary<string, object>(), routingKey, payload, "string")
+    public PublishInfo(string routingKey, string payload)
+        : this(new Dictionary<string, object>(), routingKey, payload, PayloadEncoding.String)
     {
     }
 }
