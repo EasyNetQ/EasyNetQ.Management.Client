ebc77a69e37a8af1ec9d59f27f7a5c5645c9e17d
Yury Pliner <yury.pliner@gmail.com>
2022-12-27 08:44:11 +0000

    Add approval tests

---
+++ Source/EasyNetQ.Management.Client.ApprovalTests/ApprovalTests.cs
@@ -0,0 +1,22 @@
+using PublicApiGenerator;
+using Shouldly;
+using Xunit;
+
+namespace EasyNetQ.Management.Client.ApprovalTests;
+
+public class ApprovalTests
+{
+    [Theory]
+    [InlineData(typeof(ManagementClient))]
+    public void Public_api_should_not_be_changed_unintentionally(Type type)
+    {
+        var publicApi = type?.Assembly.GeneratePublicApi(new ApiGeneratorOptions
+        {
+            IncludeAssemblyAttributes = false,
+            WhitelistedNamespacePrefixes = new[] { "Microsoft.Extensions.DependencyInjection" },
+            ExcludeAttributes = new[] { "System.Diagnostics.DebuggerDisplayAttribute" },
+        });
+
+        publicApi.ShouldMatchApproved(options => options.WithFilenameGenerator((_, _, fileType, fileExtension) => $"{type.Assembly.GetName().Name}.{fileType}.{fileExtension}"));
+    }
+}
