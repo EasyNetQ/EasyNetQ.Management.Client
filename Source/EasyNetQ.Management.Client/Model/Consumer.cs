ccb924f895aa1bec746fb5a30e74b6cad41bcaa6
Yury Pliner <yury.pliner@gmail.com>
2023-01-02 16:03:09 +0000

    Next batch of changes

--- Source/EasyNetQ.Management.Client/Model/Consumer.cs
+++ Source/EasyNetQ.Management.Client/Model/Consumer.cs
@@ -3,18 +3,16 @@ using EasyNetQ.Management.Client.Serialization;
 
 namespace EasyNetQ.Management.Client.Model;
 
-#nullable disable
-
-public class Consumer
-{
-    public Dictionary<string, object> Arguments { get; set; }
-    public bool AckRequired { get; set; }
-    public bool Active { get; set; }
-    public string ActivityStatus { get; set; }
-    [JsonConverter(typeof(EmptyArrayAsDefaultConverter<ChannelDetail>))]
-    public ChannelDetail ChannelDetails { get; set; }
-    public string ConsumerTag { get; set; }
-    public bool Exclusive { get; set; }
-    public int PrefetchCount { get; set; }
-    public QueueName Queue { get; set; }
-}
+public record Consumer(
+    [property: JsonConverter(typeof(StringObjectReadOnlyDictionaryConverter))]
+    IReadOnlyDictionary<string, object?> Arguments,
+    bool AckRequired,
+    bool Active,
+    string ActivityStatus,
+    [property: JsonConverter(typeof(EmptyArrayAsDefaultConverter<ChannelDetail>))]
+    ChannelDetail ChannelDetails,
+    string ConsumerTag,
+    bool Exclusive,
+    int PrefetchCount,
+    QueueName Queue
+);
--- Source/EasyNetQ.Management.Client/Model/Federation.cs
+++ Source/EasyNetQ.Management.Client/Model/Federation.cs
@@ -1,20 +1,15 @@
 namespace EasyNetQ.Management.Client.Model;
 
-#nullable disable
-
-public class Federation
-{
-    public string Node { get; set; }
-    public string Exchange { get; set; }
-    public string UpstreamExchange { get; set; }
-    public string Type { get; set; }
-    public string Vhost { get; set; }
-    public string Upstream { get; set; }
-
-    public string Id { get; set; }
-    public FederationStatus Status { get; set; }
-    public string LocalConnection { get; set; }
-    public string Uri { get; set; }
-
-    public string Timestamp { get; set; }
-}
+public record Federation(
+    string Node,
+    string Exchange,
+    string UpstreamExchange,
+    string Type,
+    string Vhost,
+    string Upstream,
+    string Id,
+    FederationStatus Status,
+    string LocalConnection,
+    string Uri,
+    string Timestamp
+);
