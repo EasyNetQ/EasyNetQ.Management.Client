b7071dac3ce9f540737fe6a53057001ceb011acc
Emiliano Musso <58050537+EmilianoMusso@users.noreply.github.com>
2023-02-24 00:32:30 +0100

    Implemented CreateFederationUpstream method (#226)

---
+++ Source/EasyNetQ.Management.Client/Serialization/ObjectToStringConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EasyNetQ.Management.Client.Serialization;
+
+internal sealed class ObjectToStringConverter<T> : JsonConverter<T>
+{
+    public override void Write(Utf8JsonWriter writer, T value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(value?.ToString());
+    }
+
+    public override T? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException();
+    }
+}
