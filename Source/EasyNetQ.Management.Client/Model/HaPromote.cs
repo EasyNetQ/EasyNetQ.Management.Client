6f60947c552618580f0d35be321b573c6c337b2f
inikulshin <ilyanik@gmail.com>
2023-12-10 17:07:19 +0200

    Create HaPromote.cs

---
+++ Source/EasyNetQ.Management.Client/Model/HaPromote.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace EasyNetQ.Management.Client.Model;
+
+public enum HaPromote
+{
+    [EnumMember(Value = "when-synced")]
+    WhenSynced,
+    [EnumMember(Value = "always")]
+    Always
+}
