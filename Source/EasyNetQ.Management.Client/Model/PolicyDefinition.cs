96f2ad1e6df35ae402c0af2125702c9f5c20dad7
inikulshin <ilyanik@gmail.com>
2023-12-10 16:31:15 +0200

    Update PolicyDefinition.cs

--- Source/EasyNetQ.Management.Client/Model/PolicyDefinition.cs
+++ Source/EasyNetQ.Management.Client/Model/PolicyDefinition.cs
@@ -4,6 +4,25 @@ namespace EasyNetQ.Management.Client.Model;
 
 public record PolicyDefinition
 (
+    // Queues [All types]
+    [property: JsonPropertyName("max-length")]
+    uint? MaxLength = null,
+    [property: JsonPropertyName("max-length-bytes")]
+    long? MaxLengthBytes = null,
+    [property: JsonPropertyName("overflow")]
+    Overflow? Overflow = null,
+    [property: JsonPropertyName("expires")]
+    uint? Expires = null,
+    [property: JsonPropertyName("dead-letter-exchange")]
+    string? DeadLetterExchange = null,
+    [property: JsonPropertyName("dead-letter-routing-key")]
+    string? DeadLetterRoutingKey = null,
+    [property: JsonPropertyName("message-ttl")]
+    uint? MessageTtl = null,
+    [property: JsonPropertyName("consumer-timeout")]
+    uint? ConsumerTimeout = null,
+
+    // Queues [Classic]
     [property: JsonPropertyName("ha-mode")]
     HaMode? HaMode = null,
     [property: JsonPropertyName("ha-params")]
@@ -12,22 +31,39 @@ public record PolicyDefinition
     HaSyncMode? HaSyncMode = null,
     [property: JsonPropertyName("ha-sync-batch-size")]
     int? HaSyncBatchSize = null,
+    [property: JsonPropertyName("ha-promote-on-shutdown")]
+    HaPromote? HaPromoteOnShutdown = null,
+    [property: JsonPropertyName("ha-promote-on-failure")]
+    HaPromote? HaPromoteOnFailure = null,
+    [property: JsonPropertyName("queue-version")]
+    uint? QueueVersion = null,
+    [property: JsonPropertyName("queue-master-locator")]
+    QueueLocator? QueueMasterLocator = null,
+
+    // Queues [Quorum]
+    [property: JsonPropertyName("delivery-limit")]
+    uint? DeliveryLimit = null,
+    [property: JsonPropertyName("dead-letter-strategy")]
+    DeadLetterStrategy? DeadLetterStrategy = null,
+    [property: JsonPropertyName("queue-leader-locator")]
+    QueueLocator? QueueLeaderLocator = null,
+
+    // Streams
+    [property: JsonPropertyName("max-age")]
+    string? MaxAge = null,
+    [property: JsonPropertyName("stream-max-segment-size-bytes")]
+    uint? StreamMaxSegmentSizeBytes = null,
+
+    // Exchanges
+    [property: JsonPropertyName("alternate-exchange")]
+    string? AlternateExchange = null,
+
+    // Federation
     [property: JsonPropertyName("federation-upstream")]
     string? FederationUpstream = null,
     [property: JsonPropertyName("federation-upstream-set")]
     string? FederationUpstreamSet = null,
-    [property: JsonPropertyName("alternate-exchange")]
-    string? AlternateExchange = null,
-    [property: JsonPropertyName("dead-letter-exchange")]
-    string? DeadLetterExchange = null,
-    [property: JsonPropertyName("dead-letter-routing-key")]
-    string? DeadLetterRoutingKey = null,
+
     [property: JsonPropertyName("queue-mode")]
-    string? QueueMode = null,
-    [property: JsonPropertyName("message-ttl")]
-    uint? MessageTtl = null,
-    [property: JsonPropertyName("expires")]
-    uint? Expires = null,
-    [property: JsonPropertyName("max-length")]
-    uint? MaxLength = null
+    string? QueueMode = null
 );
