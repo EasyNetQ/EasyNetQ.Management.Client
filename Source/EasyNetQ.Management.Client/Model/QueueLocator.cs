9758935aed5551dd328b99572def9c4acfb03171
inikulshin <ilyanik@gmail.com>
2023-12-10 17:09:18 +0200

    Create QueueLocator.cs

---
+++ Source/EasyNetQ.Management.Client/Model/QueueLocator.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace EasyNetQ.Management.Client.Model;
+
+public enum QueueLocator
+{
+    [EnumMember(Value = "client-local")]
+    ClientLocal,
+    [EnumMember(Value = "balanced")]
+    Balanced
+}
