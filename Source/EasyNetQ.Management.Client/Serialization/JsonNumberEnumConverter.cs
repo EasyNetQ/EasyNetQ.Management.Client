2eb304921a219ed2283e1cd3f2fa13622d84b1d4
inikulshin <ilyanik@gmail.com>
2023-12-11 13:14:34 +0200

    Create JsonNumberEnumConverter.cs

---
+++ Source/EasyNetQ.Management.Client/Serialization/JsonNumberEnumConverter.cs
@@ -0,0 +1,38 @@
+#if !NET8_0_OR_GREATER
+
+namespace System.Text.Json.Serialization;
+
+internal sealed class JsonNumberEnumConverter<TEnum> : JsonConverter<TEnum> where TEnum : struct, Enum
+{
+    private static readonly TypeCode s_enumTypeCode = Type.GetTypeCode(typeof(TEnum));
+    static JsonNumberEnumConverter()
+    {
+        switch (s_enumTypeCode)
+        {
+            case TypeCode.Int32: break;
+            default: throw new NotImplementedException($"{typeof(JsonNumberEnumConverter<TEnum>)}: underlying type {s_enumTypeCode} is not supported");
+        }
+    }
+
+    public JsonNumberEnumConverter() { }
+
+    public override TEnum Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        switch (s_enumTypeCode)
+        {
+            case TypeCode.Int32: return (TEnum)(object)reader.GetInt32();
+            default: throw new NotImplementedException();
+        }
+    }
+
+    public override void Write(Utf8JsonWriter writer, TEnum value, JsonSerializerOptions options)
+    {
+        switch (s_enumTypeCode)
+        {
+            case TypeCode.Int32: writer.WriteNumberValue((int)(object)value); break;
+            default: throw new NotImplementedException();
+        }
+    }
+}
+
+#endif
