7df6ce3e93642f72587d7ff622885ef3daad727b
inikulshin <ilyanik@gmail.com>
2023-12-11 10:29:42 +0200

    Update JsonStringEnumConverterEx.cs

--- Source/EasyNetQ.Management.Client/Serialization/JsonStringEnumConverterEx.cs
+++ Source/EasyNetQ.Management.Client/Serialization/JsonStringEnumConverterEx.cs
@@ -2,54 +2,53 @@ using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 using System.Text.Json;
 
-namespace EasyNetQ.Management.Client.Serialization
+namespace EasyNetQ.Management.Client.Serialization;
+
+// https://github.com/dotnet/runtime/issues/31081#issuecomment-848697673
+public class JsonStringEnumConverterEx<TEnum> : JsonConverter<TEnum> where TEnum : struct, Enum
 {
-    public class JsonStringEnumConverterEx<TEnum> : JsonConverter<TEnum> where TEnum : struct, System.Enum
+    private readonly Dictionary<TEnum, string> _enumToString = new Dictionary<TEnum, string>();
+    private readonly Dictionary<string, TEnum> _stringToEnum = new Dictionary<string, TEnum>();
+
+    public JsonStringEnumConverterEx()
     {
+        var type = typeof(TEnum);
+        var values = Enum.GetValues(type);
 
-        private readonly Dictionary<TEnum, string> _enumToString = new Dictionary<TEnum, string>();
-        private readonly Dictionary<string, TEnum> _stringToEnum = new Dictionary<string, TEnum>();
-
-        public JsonStringEnumConverterEx()
+        foreach (TEnum value in values)
         {
-            var type = typeof(TEnum);
-            var values = System.Enum.GetValues(type);
+            var enumMember = type.GetMember(value.ToString())[0];
+            var attr = enumMember.GetCustomAttributes(typeof(EnumMemberAttribute), false)
+              .Cast<EnumMemberAttribute>()
+              .FirstOrDefault();
 
-            foreach (TEnum value in values)
+            if (attr?.Value != null)
             {
-                var enumMember = type.GetMember(value.ToString())[0];
-                var attr = enumMember.GetCustomAttributes(typeof(EnumMemberAttribute), false)
-                  .Cast<EnumMemberAttribute>()
-                  .FirstOrDefault();
-
-                if (attr?.Value != null)
-                {
-                    _enumToString.Add(value, attr.Value);
-                    _stringToEnum.Add(attr.Value, value);
-                }
-                else
-                {
-                    _enumToString.Add(value, value.ToString());
-                    _stringToEnum.Add(value.ToString(), value);
-                }
+                _enumToString.Add(value, attr.Value);
+                _stringToEnum.Add(attr.Value, value);
+            }
+            else
+            {
+                _enumToString.Add(value, value.ToString());
+                _stringToEnum.Add(value.ToString(), value);
             }
         }
+    }
 
-        public override TEnum Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    public override TEnum Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var stringValue = reader.GetString();
+
+        if (stringValue != null && _stringToEnum.TryGetValue(stringValue, out var enumValue))
         {
-            var stringValue = reader.GetString();
-
-            if (stringValue != null && _stringToEnum.TryGetValue(stringValue, out var enumValue))
-            {
-                return enumValue;
-            }
-
-            return default;
+            return enumValue;
         }
 
-        public override void Write(Utf8JsonWriter writer, TEnum value, JsonSerializerOptions options)
-        {
-            writer.WriteStringValue(_enumToString[value]);
-        }
+        return default;
+    }
+
+    public override void Write(Utf8JsonWriter writer, TEnum value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(_enumToString[value]);
     }
 }
