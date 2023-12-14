36612873db9810544563fa35030a4d7811dd74ed
inikulshin <ilyanik@gmail.com>
2023-12-14 10:43:47 +0200

    Update Queue.cs

--- Source/EasyNetQ.Management.Client/Model/Queue.cs
+++ Source/EasyNetQ.Management.Client/Model/Queue.cs
@@ -1,33 +1,39 @@
 using System.Text.Json.Serialization;
-using EasyNetQ.Management.Client.Serialization;
 
 namespace EasyNetQ.Management.Client.Model;
 
 public record Queue(
+    string Name,
+    string Vhost,
+    QueueType Type,
+    string Node,
+    string? State,
+    IReadOnlyDictionary<string, object?> Arguments,
+    bool Durable,
+    bool Exclusive,
+    bool AutoDelete,
+    long MessagesReady,
+    long MessagesUnacknowledged,
+    long Messages,
+
     long Memory,
     string? IdleSince,
     string? Policy,
     string? ExclusiveConsumerTag,
-    long MessagesReady,
-    long MessagesUnacknowledged,
-    long Messages,
+    long MessageBytes,
     long Consumers,
     long ActiveConsumers,
     BackingQueueStatus? BackingQueueStatus,
     IReadOnlyList<ConsumerDetail>? ConsumerDetails,
     long? HeadMessageTimestamp,
-    string Name,
-    string Vhost,
-    bool Durable,
-    bool Exclusive,
-    bool AutoDelete,
-    [property: JsonConverter(typeof(StringObjectReadOnlyDictionaryConverter))]
-    IReadOnlyDictionary<string, object?> Arguments,
-    string? Node,
     IReadOnlyList<string>? SlaveNodes,
     IReadOnlyList<string>? SynchronisedSlaveNodes,
     LengthsDetails? MessagesDetails,
     LengthsDetails? MessagesReadyDetails,
     LengthsDetails? MessagesUnacknowledgedDetails,
     MessageStats? MessageStats
-);
+) : QueueWithoutStats(Name, Vhost, Type, Node, State, Arguments, Durable, Exclusive, AutoDelete, MessagesReady, MessagesUnacknowledged, Messages)
+{
+    [JsonExtensionData()]
+    public Dictionary<string, object>? ExtensionData { get; set; }
+};
