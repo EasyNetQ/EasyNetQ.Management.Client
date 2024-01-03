19e8164aa5ec19230883fef2726f621b714c6bd9
inikulshin <ilyanik@gmail.com>
2024-01-03 23:04:20 +0200

    Add more info to UnexpectedHttpStatusCodeException

--- Source/EasyNetQ.Management.Client/Internals/HttpResponseMessageExtensions.cs
+++ Source/EasyNetQ.Management.Client/Internals/HttpResponseMessageExtensions.cs
@@ -7,6 +7,6 @@ internal static class HttpResponseMessageExtensions
     public static void EnsureExpectedStatusCode(this HttpResponseMessage response, Func<HttpStatusCode, bool> isExpected)
     {
         if (!isExpected(response.StatusCode))
-            throw new UnexpectedHttpStatusCodeException(response.StatusCode);
+            throw new UnexpectedHttpStatusCodeException(response);
     }
 }
