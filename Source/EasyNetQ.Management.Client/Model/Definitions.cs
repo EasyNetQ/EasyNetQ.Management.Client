4e21241ca927412419477aa9b67a73804de8c2fa
inikulshin <ilyanik@gmail.com>
2024-01-03 21:23:55 +0200

    Add missing properties to Definitions (#298)

--- Source/EasyNetQ.Management.Client/Model/Definitions.cs
+++ Source/EasyNetQ.Management.Client/Model/Definitions.cs
@@ -1,11 +1,33 @@
-﻿namespace EasyNetQ.Management.Client.Model;
+﻿using EasyNetQ.Management.Client.Serialization;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace EasyNetQ.Management.Client.Model;
 
 public record Definitions(
     string RabbitVersion,
+    string RabbitmqVersion,
+    string ProductName,
+    string ProductVersion,
     IReadOnlyList<User> Users,
     IReadOnlyList<Vhost> Vhosts,
     IReadOnlyList<Permission> Permissions,
+    IReadOnlyList<TopicPermission> TopicPermissions,
+    IReadOnlyList<Parameter> Parameters,
+    IReadOnlyList<GlobalParameter> GlobalParameters,
+    IReadOnlyList<Policy> Policies,
     IReadOnlyList<Queue> Queues,
     IReadOnlyList<Exchange> Exchanges,
     IReadOnlyList<Binding> Bindings
-);
+)
+{
+    [JsonExtensionData()]
+    public IDictionary<string, JsonElement>? JsonExtensionData { get; set; }
+
+    [JsonIgnore()]
+    public IReadOnlyDictionary<string, object?>? ExtensionData
+    {
+        get { return JsonExtensionDataExtensions.ToExtensionData(JsonExtensionData); }
+        set { JsonExtensionData = JsonExtensionDataExtensions.ToJsonExtensionData(value); }
+    }
+};
